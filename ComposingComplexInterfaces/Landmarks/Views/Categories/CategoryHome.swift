06f111cca343a6bf3ccbf39d4bd83e68bb91f0f8
michael2k <mskim2k@gmail.com>
2022-10-16 22:37:46 +0900

    completed section 1 Add a Category View

---
+++ ComposingComplexInterfaces/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHOme.swift
+//  Landmarks
+//
+//  Created by Michael M. Kim on 2022/10/16.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHOme: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHOme_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHOme()
+    }
+}
