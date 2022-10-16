f8f2f7fc1f414f741a95c235c0853cee54539a78
michael2k <mskim2k@gmail.com>
2022-10-16 11:30:10 +0900

    completed section 4 Create the List of Landmarks

---
+++ StartingPoint/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Michael M. Kim on 2022/10/16.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
