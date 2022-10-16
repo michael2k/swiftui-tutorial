60209cf29c0b7b0d278ec07d40bb4ee3f9fbc23f
michael2k <mskim2k@gmail.com>
2022-10-16 15:14:53 +0900

    completed section Filter the List View

--- HandlingUserInput/Landmarks/Views/LandmarkList.swift
+++ HandlingUserInput/Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,17 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFacoriteOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { Landmark in
+            (!showFacoriteOnly || Landmark.isFavorite )
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
@@ -24,10 +32,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
     }
 }
