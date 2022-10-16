3bb7c809262d37ab883c71acdf34c63d0e395a31
michael2k <mskim2k@gmail.com>
2022-10-16 11:04:55 +0900

    completed section 1 of Building Lists and Navigation

---
+++ StartingPoint/Model/Landmark.swift
@@ -0,0 +1,37 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Michael M. Kim on 2022/10/16.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.latitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ StartingPoint/Model/ModelData.swift
@@ -0,0 +1,33 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Michael M. Kim on 2022/10/16.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import Foundation
+
+var andmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename:String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't fine \(filename) in main bundle)")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from man bundle: \n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self): \n\(error)")
+    }
+}
