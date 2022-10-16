4ab8cde38730b25cec08e3e5d226a46cad2273e7
michael2k <mskim2k@gmail.com>
2022-10-17 08:22:29 +0900

    added Interfacing with UIKit

---
+++ InterfacingWithUIKit/Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,26 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Michael M. Kim on 2022/10/17.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map {
+            FeatureCard(landmark: $0)
+                .aspectRatio(3 / 2, contentMode: .fit)
+        })
+    }
+}
---
+++ InterfacingWithUIKit/Landmarks/Views/PageView/PageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Michael M. Kim on 2022/10/17.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
