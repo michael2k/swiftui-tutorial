fc416ff393319a6f6ec8093b5ddce7cab11cfce7
michael2k <mskim2k@gmail.com>
2022-10-19 23:56:27 +0900

    completed section 4 Add the Landmarks List

---
+++ CreatingAwatchOSApp/WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Michael M. Kim on 2022/10/19.
+//  Copyright © 2022 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
